f4c59b8107012ded0db39dfbc2cb0f201ff0107f
HemanthRaghu96 <hemanthraghu96@gmail.com>
2024-08-15 14:43:47 +0530

    Created Navigate using useRouter Hook

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,18 @@
-import Image from "next/image";
+'use client'
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  let router=useRouter()
   return (
     <main className="flex min-h-screen flex-col items-center  p-24">
-     <h1>Welcome</h1>
+     <h1 className="font-bold text-5xl">Welcome</h1>
+     <h1 className="font-semibold text-2xl mt-4">Navigate using Link</h1>
      <Link href={'clientComponent'}>Go to clientComponent</Link>
      <Link href={'serverComponent'}>Go to serverComponent</Link>
+     <h1 className="font-semibold text-2xl mt-4">Navigate using useRouter</h1>
+    <button onClick={()=>router.push('clientComponent')}>Go to clientComponent</button>
+    <button onClick={()=>router.push('serverComponent')}>Go to serverComponent</button>
     </main>
   );
 }
