eedba813a27eaeb5308a06aafa1693f80176143f
HemanthRaghu96 <hemanthraghu96@gmail.com>
2024-08-15 19:05:53 +0530

    Created Loading UI

--- src/app/serverComponent/product/[detailes]/page.js
+++ src/app/serverComponent/product/[detailes]/page.js
@@ -1,4 +1,10 @@
-export default function ProductDetailes({searchParams}){
+import Loading from "@/app/loading"
+import { Suspense } from "react"
+
+export default function ProductDetailes({searchParams,children}){
     console.log(searchParams.search)
-    return <h1>This is product detailes page</h1>
+    return <>
+    <h1>This is product detailes page</h1>
+          <Suspense fallback={<Loading />}>{children}</Suspense>
+    </>
 }
